c7e5a01642e6758783106d48b54c2cc11823ed0d
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-14 12:55:20 +0300

    refactor: the OrderRepository class

--- src/main/java/com/pulsar/repository/OrderRepository.java
+++ src/main/java/com/pulsar/repository/OrderRepository.java
@@ -2,24 +2,28 @@ package com.pulsar.repository;
 
 import com.pulsar.model.Order;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class OrderRepository {
 
     private static int id = 1;
+    private final Map<Integer, Order> orders;
+
+    public OrderRepository() {
+        this.orders = new HashMap<>();
+    }
 
     public int save(Order order) {
-        if (order == null || order.getProductName() == null) {
+        if (order == null) {
             return -1;
         }
-        return ++id;
+        orders.put(id, order);
+        return id++;
     }
 
     public Optional<Order> findById(Integer orderId) {
-        if (orderId < 1) {
-            return Optional.empty();
-        }
-        return Optional.of(new Order(orderId, "TEST", 10, BigDecimal.valueOf(100)));
+        return Optional.ofNullable(orders.get(orderId));
     }
 }
