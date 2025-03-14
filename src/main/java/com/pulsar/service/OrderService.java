da06805bc852ea1a92de3d4a9dedd818b5227d0a
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-14 12:58:08 +0300

    add: implement the processOrder() method in the OrderService class

--- src/main/java/com/pulsar/service/OrderService.java
+++ src/main/java/com/pulsar/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.pulsar.service;
 
+import com.pulsar.model.Order;
 import com.pulsar.repository.OrderRepository;
 
 public class OrderService {
@@ -8,5 +9,10 @@ public class OrderService {
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
+    }
+
+    public String processOrder(Order order) {
+        int orderId = orderRepository.save(order);
+        return (orderId != -1) ? "Order processed successfully" : "Order processing failed";
     }
 }
