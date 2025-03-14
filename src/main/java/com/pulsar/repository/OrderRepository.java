2a9e49c893447184d86d0a89ca97e55fc00c7c23
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-14 12:51:07 +0300

    add: implement the findById() method in the OrderRepository class

--- src/main/java/com/pulsar/repository/OrderRepository.java
+++ src/main/java/com/pulsar/repository/OrderRepository.java
@@ -2,6 +2,9 @@ package com.pulsar.repository;
 
 import com.pulsar.model.Order;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class OrderRepository {
 
     private static int id = 1;
@@ -11,5 +14,12 @@ public class OrderRepository {
             return -1;
         }
         return ++id;
+    }
+
+    public Optional<Order> findById(Integer orderId) {
+        if (orderId < 1) {
+            return Optional.empty();
+        }
+        return Optional.of(new Order(orderId, "TEST", 10, BigDecimal.valueOf(100)));
     }
 }
