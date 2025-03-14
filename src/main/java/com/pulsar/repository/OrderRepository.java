ff9947b91372ce21d62713a3143c6cfdc38ef95a
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-14 12:50:39 +0300

    add: implement the save() method in the OrderRepository class

--- src/main/java/com/pulsar/repository/OrderRepository.java
+++ src/main/java/com/pulsar/repository/OrderRepository.java
@@ -1,5 +1,15 @@
 package com.pulsar.repository;
 
+import com.pulsar.model.Order;
+
 public class OrderRepository {
-    
+
+    private static int id = 1;
+
+    public int save(Order order) {
+        if (order == null || order.getProductName() == null) {
+            return -1;
+        }
+        return ++id;
+    }
 }
