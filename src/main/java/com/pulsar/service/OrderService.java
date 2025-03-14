55822d1c874177d7605042a851d515cfebc8c267
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-14 13:02:03 +0300

    add: implement the calculateTotal() method in the OrderService class

--- src/main/java/com/pulsar/service/OrderService.java
+++ src/main/java/com/pulsar/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.pulsar.service;
 
+import com.pulsar.exception.OrderNotFoundException;
 import com.pulsar.model.Order;
 import com.pulsar.repository.OrderRepository;
+
+import java.math.BigDecimal;
 
 public class OrderService {
 
@@ -14,5 +17,11 @@ public class OrderService {
     public String processOrder(Order order) {
         int orderId = orderRepository.save(order);
         return (orderId != -1) ? "Order processed successfully" : "Order processing failed";
+    }
+
+    public BigDecimal calculateTotal(int orderId) {
+        return orderRepository.findById(orderId)
+                .map(Order::getTotalPrice)
+                .orElseThrow(OrderNotFoundException::new);
     }
 }
